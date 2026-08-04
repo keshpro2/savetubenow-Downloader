d0fe1a7ce1b7de9494a0f9ce1f4d3db4428b92b5
shimo.com <shimokevin@gmail.com>
2026-08-04 17:13:18 +0200

    my update  to the website

--- server.js
+++ server.js
@@ -44,6 +44,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || 'rukundoshimokevin',
     database: process.env.DB_NAME || 'savetubenow',
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: true } : false ,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
